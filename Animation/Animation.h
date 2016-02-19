250123fdb235a722ceba8035af95a9c899427611
christian otkjær <christian.otkjaer@gmail.com>
2016-02-19 09:02:53 +0100

    Initial Commit

---
+++ Animation/Animation.h
@@ -0,0 +1,19 @@
+//
+//  Animation.h
+//  Animation
+//
+//  Created by Christian Otkjær on 19/02/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Animation.
+FOUNDATION_EXPORT double AnimationVersionNumber;
+
+//! Project version string for Animation.
+FOUNDATION_EXPORT const unsigned char AnimationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Animation/PublicHeader.h>
+
+
